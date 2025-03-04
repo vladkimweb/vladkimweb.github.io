258fc72a118e88048ccda678ff5ed87ae6eb9622
Vladislav Kim <vladislavkim@MacBook-Air-Vladislav.local>
2025-03-04 17:34:26 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('header').addEventListener('mouseover', () => {
+  document.querySelector('header').style.backgroundColor = '#00796b'
+})
+
+document.querySelector('header').addEventListener('mouseout', () => {
+  document.querySelector('header').style.backgroundColor = '#333'
+})
